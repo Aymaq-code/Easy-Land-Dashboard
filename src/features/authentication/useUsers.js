7023ae3eb0a69beb00ed28c8a767743f3481a2ed
Aymaq <rashidaymaq1994@gmail.com>
2026-05-06 17:27:24 +0430

    auth

--- src/features/authentication/useUsers.js
+++ src/features/authentication/useUsers.js
@@ -1,9 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
-const API_BASE_URL = "http://10.0.30.13:3000";
-
 async function getUsers() {
-  const res = await fetch(`${API_BASE_URL}/users`);
+  const res = await fetch("http://localhost:3000/users");
 
   if (!res.ok) throw new Error("Failed to fetch users");
 
